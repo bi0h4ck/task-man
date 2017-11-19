9852a17f71bd91296961fc508beb96f796173d15
bi0h4ck <juliediempham@gmail.com>
2017-11-18 18:29:03 -0600

    create funtions to create and delete a user

--- src/main/scala/us/diempham/taskman/web/services/User.scala
+++ src/main/scala/us/diempham/taskman/web/services/User.scala
@@ -1,6 +1,20 @@
 package us.diempham.taskman.web.services
 
+import scala.collection.mutable
+
 object User {
   case class User(id: String, email: String, password: String)
+
+  val database: mutable.Map[String, User] = mutable.Map.empty[String, User]
+
+  def createUser(id: String, email: String, password: String) = {
+    database += (id -> User(id, email, password))
+  }
+
+  def deleteUser(id: String) = {
+    database -= id
+  }
+
+  
 
 }
